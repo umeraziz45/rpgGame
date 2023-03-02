a6fe3904f0b6b59b0b52664df6139b79a8d0f4a0
umeraziz45 <88170471+umeraziz45@users.noreply.github.com>
2023-03-01 18:47:46 -0700

    refactored html and dice score generation

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
 const hero = {
-  className: '.hero',
+  className: 'hero',
   charName: 'Wizard',
   avatar: 'wizard',
   health: '60',
-  diceScore: 6
+  diceScore: [1,2,3],
+  diceCount: 3
 }
 
+// const {}
+
 const monster = {
-  className: '.monster',
+  className: 'monster',
   charName: 'Orc',
   avatar: 'orc',
   health: '10',
-  diceScore: 4
+  diceScore: [4],
+  diceCount: 1
+}
+
+function getDiceRollArray(diceCount) {
+  let newDiceRolls = [];
+  for (let i = 0; i < diceCount; i++) {
+      newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+  }
+  return newDiceRolls;
+}
+
+const getDiceHtml = (arr) => {
+  let diceHtml = '';
+  arr.map( (elem) => {
+      diceHtml += `<div class="dice">${elem}</div>`
+  })
+  return diceHtml;
 }
 
 const charactorCreator = (data) => {
 
-  let charInfo = 
-    document.querySelector(data.className).innerHTML =  `
+  const {className, charName, avatar, health, diceScore, diceCount} = data;
+
+  let diceHtml = getDiceHtml(getDiceRollArray(diceCount));
+
+    document.querySelector('.'+className).innerHTML =  `
     <div class="character-card">
-      <h4 class="character-name">${data.charName}</h4>
-      <img src="./assets/${data.avatar}.png" alt="image of wizard" class="character-avatar">
-      <p class="character-health"> health: <b>${data.health}</b></p>
-      <div class="character-dice-container"><div class="character-dice">${data.diceScore}</div></div>
+      <h4 class="character-name">${charName}</h4>
+      <img src="./assets/${avatar}.png" alt="image of wizard" class="character-avatar">
+      <p class="character-health"> health: <b>${health}</b></p>
+      <div class="character-dice-container"> 
+      ${diceHtml}
+      <div 
     </div>
    `
-  return charInfo;
 }
 
 charactorCreator(hero);
